222690073384a459a45af7e6fad85663207001c4
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2024-07-12 20:52:35 +0200

    Update script.js
    Final submisison of project. We regluar expression to search for pattern. We also use If else statements for our results.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    let validation = /^pet_(?:[\d]+[\w]+|[\w]+[\d]+)$/ // Validation logic goes here
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
+    if(validation.test(input)) {
+        let result = 'Valid Syntax';
+        document.getElementById('result').innerText = result;
+    } else {
+        let result = 'InValid Syntax';
+        document.getElementById('result').innerText = result;
+
+    }
+
+    }
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
-}
+          //  document.getElementById('result').innerText = result;
+// }
 
 
